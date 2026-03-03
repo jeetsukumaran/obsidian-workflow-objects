d8604751798f58daae4ca64b92557e3595bea071
Jeet Sukumaran <jeetsukumaran@gmail.com>
2026-03-02 19:35:52 -0800

    Linter config matches GitHub review

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,25 @@
-// eslint.config.mjs
+// eslint.config.js
 import tsparser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
 import obsidianmd from "eslint-plugin-obsidianmd";
 
 export default defineConfig([
   ...obsidianmd.configs.recommended,
-  // ...obsidianmd.configs.recommendedWithLocalesEn,
   {
     files: ["**/*.ts"],
     languageOptions: {
       parser: tsparser,
       parserOptions: { project: "./tsconfig.json" },
     },
-
     rules: {
+      "obsidianmd/ui/sentence-case": "error",
+      "@typescript-eslint/restrict-template-expressions": "error",
+      "@typescript-eslint/no-base-to-string": "error",
+
+      "no-undef": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
     },
   },
 ]);
-
